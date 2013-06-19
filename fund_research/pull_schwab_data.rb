d3b13e188847f2a4a490b08a47f0243c7b3187b7
gazzman <sgazzisg@uw>
2013-06-19 06:34:00 -0400

    Added Watir::Wait::TimeoutError to rescue clause.

--- fund_research/pull_schwab_data.rb
+++ fund_research/pull_schwab_data.rb
@@ -78,7 +78,7 @@ def get_data(user, pass, sd, symbols, progname=nil)
             headers.each {|header|
                 if header.index('價') then raise NameError, "Asian characters; retrying" end
             }
-        rescue Timeout::Error, Errno::ETIMEDOUT, *errs => err
+        rescue Timeout::Error, Errno::ETIMEDOUT, Watir::Wait::TimeoutError, *errs => err
             try += 1
             e_msg = "Exception " + err.class.to_s
             e_msg += " raised with message \'" + err.to_s
